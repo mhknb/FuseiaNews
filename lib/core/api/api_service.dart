3539e08d7c0217ba8a5c325da81b68ed257ae563
abdurresitdemir <abdurresitdemir@gmail.com>
2025-07-14 13:12:08 +0300

    kişiye ozel haberler ve bircok api servise bitti

--- lib/core/api/api_service.dart
+++ lib/core/api/api_service.dart
@@ -4,26 +4,20 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:webfeed_plus/webfeed_plus.dart'; // webfeed_plus'ı import ediyoruz
 import '../models/news_model.dart';
+import '../utilis/constants.dart';
 import 'youtube_service.dart'; // Yeni YouTube servisimizi import ediyoruz
 
 class RssService {
   final YoutubeService _youtubeService = YoutubeService();
 
-  // Artık sadece RSS linklerini tutuyoruz.
-  final Map<String, String> _interestToRssMap = {
-    'Teknoloji': 'http://feeds.bbci.co.uk/news/technology/rss.xml',
-    'Bilim': 'http://feeds.bbci.co.uk/news/science_and_environment/rss.xml',
-    'Sağlık': 'http://feeds.bbci.co.uk/news/health/rss.xml',
-    'Spor': 'https://feeds.skynews.com/feeds/rss/sports.xml',
-    'Gündem': 'http://feeds.bbci.co.uk/news/rss.xml',
-    //buraya eklenecek unutma
-  };
+
 
 
 
 
   Future<List<HaberModel>> fetchGlobalNews() {
-    return _fetchNewsFromUrl('http://feeds.bbci.co.uk/news/technology/rss.xml');
+    // Artık URL'yi doğrudan yazmıyoruz, sabit dosyasından çekiyoruz.
+    return _fetchNewsFromUrl(kGlobalNewsUrl);
   }
 
 
